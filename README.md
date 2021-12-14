024541e9d8d7bdd33a6c50f1501e4719086c13a4
Chrysanthemum Lovelace <chryslovelace@gmail.com>
2021-12-14 16:21:18 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## ezp-log-to-csv
+
+This console application takes a path to an EZProxy log file as an argument and writes a CSV representation of it to stdout.
+
+## License
+
+Copyright University of Pittsburgh.
+
+Freely licensed for reuse under the MIT License.
---
+++ src/main.rs
@@ -0,0 +1,55 @@
+use anyhow::{Context, Result};
+use std::{
+    fs::File,
+    io::{stdout, BufRead, BufReader},
+    path::PathBuf,
+};
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Options {
+    #[structopt(parse(from_os_str))]
+    file: PathBuf,
+    #[structopt(short, long = "include-headers", help = "Include CSV headers")]
+    headers: bool,
+}
+
+fn main() -> Result<()> {
+    let options = Options::from_args();
+    let mut input = BufReader::new(
+        File::open(&options.file).context(format!("failed to open {:?}", options.file))?,
+    );
+    let mut writer = csv::Writer::from_writer(stdout());
+    if options.headers {
+        writer
+            .write_record(&[
+                "IP Address",
+                "Username",
+                "Session ID",
+                "Timestamp",
+                "Timezone",
+                "HTTP Method",
+                "Request URL",
+                "HTTP Version",
+                "HTTP Status Code",
+                "Bytes Transferred",
+            ])
+            .context("failed to write csv headers")?;
+    }
+    let mut buf = String::new();
+    while input
+        .read_line(&mut buf)
+        .context("failed to read a line from the input file")?
+        != 0
+    {
+        writer
+            .write_record(
+                buf.trim()
+                    .split(' ')
+                    .map(|field| field.trim_matches(&['"', '[', ']'][..])),
+            )
+            .context("failed to write a csv record")?;
+        buf.clear();
+    }
+    Ok(())
+}
